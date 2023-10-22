f89b7218da6d921a12d909ba85759e4599f158ef
Muhammad Sabeeh <muhammadsabeeh01@gmail.com>
2023-10-22 03:51:52 -0400

    supabase setup, works perfectly

---
+++ fastapi_backend/server/supabase_client.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from supabase import create_client, Client
+import os
+
+load_dotenv()
+
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
--- fastapi_backend/server/user_routes.py
+++ fastapi_backend/server/user_routes.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, Body
 from pydantic import BaseModel, EmailStr
+import json
+
+from supabase_client import supabase
 
 router = APIRouter(
     prefix = "/users",
@@ -15,6 +18,22 @@ class User(BaseModel):
 @router.get("/")
 async def get_users():
     return [{"name": "Harry"}, {"name": "Ron"}]
+
+@router.get("/pets")
+async def get_pets():
+    response = supabase.table("pets").select("*").execute()
+    
+    
+    # check if redis has same data, return data as json
+    if r.exists("pets"):
+        print("redis")
+        data = r.get("pets")
+        return json.loads(data)
+    
+    # add data to redis
+    responseString = json.dumps(response.data)
+    r.set("pets", responseString)
+    return response.data
 
 @router.post("/add")
 async def add_user(user: User = Body(...)):
